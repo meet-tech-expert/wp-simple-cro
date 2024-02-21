bf545b1a1123de66e01fc06357b4b53a10e4ed94
Shivani Gupta <shivaniguptas206@gmail.com>
2024-02-21 19:30:16 +0530

    Create a simple cro block

--- admin/class-wp-simple-cro-admin.php
+++ admin/class-wp-simple-cro-admin.php
@@ -61,18 +61,6 @@ class Wp_Simple_Cro_Admin {
 	 */
 	public function enqueue_styles() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Wp_Simple_Cro_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Wp_Simple_Cro_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wp-simple-cro-admin.css', array(), $this->version, 'all' );
 
 	}
@@ -83,21 +71,35 @@ class Wp_Simple_Cro_Admin {
 	 * @since    1.0.0
 	 */
 	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Wp_Simple_Cro_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Wp_Simple_Cro_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wp-simple-cro-admin.js', array( 'jquery' ), $this->version, false );
-
+		wp_enqueue_script( 
+			$this->plugin_name, 
+			plugin_dir_url( __FILE__ ) . 'js/wp-simple-cro-admin.js', 
+			array('jQuery'), 
+			$this->version, 
+			false 
+		);
+		wp_enqueue_script( 
+			'simple-cro-gutenberg', 
+			plugin_dir_url( __FILE__ ) . 'js/simple-cro.js', 
+			array('wp-blocks', 'wp-editor'), 
+			$this->version, 
+			true 
+		);
+	
+		// Localize script for translation
+		wp_localize_script(
+			$this->plugin_name,
+			'simpleCroBlock', // Handle to access localized data in JavaScript
+			array(
+				'ajax_url' => admin_url( 'admin-ajax.php' ), // Example of localized data
+			)
+		);
+	
+		// Set script translations
+		wp_set_script_translations( 
+			'wp-simple-cro-admin', 
+			'wp-simple-cro', 
+			plugin_dir_path( __FILE__ ) . 'languages' 
+		);
 	}
-
 }
---
+++ admin/js/simple-cro.js
@@ -0,0 +1,120 @@
+(function( $ ) {
+    'use strict';
+    var __ = wp.i18n.__;
+    var el = wp.element.createElement;
+    var registerBlockType = wp.blocks.registerBlockType;
+    var InspectorControls = wp.blockEditor.InspectorControls;
+    var Button = wp.components.Button;
+    var TextControl = wp.components.TextControl;
+    var RangeControl = wp.components.RangeControl;
+
+    registerBlockType( 'simple-cro/block', {
+        title: __( 'Simple CRO', 'wp-simple-cro' ),
+        icon: 'admin-tools',
+        category: 'layout',
+        className: 'simple-cro',
+
+        attributes: {
+            isNewCRO: {
+                type: 'boolean',
+                default: false
+            },
+            croTestTitle: {
+                type: 'string',
+                default: ''
+            },
+            croCategory: {
+                type: 'string',
+                default: ''
+            },
+            croBlockUniqueId: {
+                type: 'string',
+                default: ''
+            },
+            blockFrequency: {
+                type: 'number',
+                default: 50
+            }
+        },
+
+        edit: function( props ) {
+            const { attributes, setAttributes } = props;
+            const { isNewCRO } = attributes;
+
+            const handleNewButtonClick = () => {
+                setAttributes({ isNewCRO: true });
+            };
+
+            const handleExistingButtonClick = () => {
+                setAttributes({ isNewCRO: false });
+            };
+
+            return [
+               
+                // Buttons to switch between New CRO and Existing CRO
+                el(
+                    'div',
+                    { className: 'simple-cro-buttons' },
+                    el(
+                        'h3',
+                        { className: props.className },
+                        ' Simple CRO',
+                    ),
+                    el(
+                        'p',
+                        { className: props.className },
+                        ' Simplify Gutenberg block testing with the Simple CRO',
+                    ),
+                    el( Button, {
+                        isPrimary: isNewCRO,
+                        onClick: handleNewButtonClick,
+                    }, 'New CRO' ),
+                    el( Button, {
+                        isPrimary: !isNewCRO,
+                        onClick: handleExistingButtonClick,
+                    }, 'Existing CRO' )
+                ),
+                // Options panel in the right sidebar
+                isNewCRO && el(
+                    InspectorControls,
+                    null,
+                    el(
+                        PanelBody,
+                        {
+                            title: 'Block Options',
+                            initialOpen: true,
+                        },
+                        el( TextControl, {
+                            label: 'CRO Test Title',
+                            value: props.attributes.croTestTitle,
+                            onChange: newValue => props.setAttributes( { croTestTitle: newValue } ),
+                        } ),
+                        el( TextControl, {
+                            label: 'CRO Category',
+                            value: props.attributes.croCategory,
+                            onChange: newValue => props.setAttributes( { croCategory: newValue } ),
+                        } ),
+                        el( TextControl, {
+                            label: 'CRO Block Unique Id',
+                            value: props.attributes.croBlockUniqueId,
+                            onChange: newValue => props.setAttributes( { croBlockUniqueId: newValue } ),
+                        } ),
+                        el( RangeControl, {
+                            label: 'Slider to adjust block frequency',
+                            value: props.attributes.blockFrequency,
+                            onChange: newValue => props.setAttributes( { blockFrequency: newValue } ),
+                            min: 0,
+                            max: 100,
+                            step: 1,
+                        } )
+                    )
+                )
+            ];            
+        },
+
+        save: () => {
+            return null; // No content saved on the front end
+        },
+    } );
+    
+})( jQuery );
